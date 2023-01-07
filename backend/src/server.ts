f8e973c520ac06902d6a4aa66405afb43455b970
sund4e <suvmakinen@gmail.com>
2023-01-07 12:04:06 +0200

    Authenticate requests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,19 @@
 import * as Koa from 'koa';
 import * as cors from '@koa/cors';
 import { fileRouter } from './routes/files';
+import * as jwt from 'koa-jwt';
 
 const app = new Koa();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+if (!process.env.JWT_SECRET) {
+  throw Error('No JWT_SECRET in env');
+}
+app.use(jwt({ secret: process.env.JWT_SECRET, cookie: 'access_token' }));
 
 app.use(fileRouter.routes());
 
-app.listen(5000);
+app.listen(process.env.PORT);
 
-console.log('Server running on port 5000');
+console.log(`Server running on port ${process.env.PORT}`);
--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,3 +1,10 @@
+//Mock jwt token, should be set as httpOnly cookie by main app login
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlN1dmkiLCJpYXQiOjE1MTYyMzkwMjJ9.RbUIocMvXHt3t1b2EPMgOG3qE4QkUgtwlNRgeP4X4Mg';
+const token2 =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwOTg3NjU0MzIxIiwibmFtZSI6IlNhbXUiLCJpYXQiOjE1MTYyMzkwMjJ9.q-QG_SqU6RBFEcs438OO7ytGtDAasuat9rSJf7JAcsc';
+document.cookie = `access_token=${token}; SameSite=Strict; Secure`;
+
 export const uploadImage = (file: File) => {
   if (!process.env.REACT_APP_BACKEND_URL) {
     throw Error('REACT_APP_BACKEND_URL missing from env');
@@ -9,5 +16,6 @@ export const uploadImage = (file: File) => {
   return fetch(`${process.env.REACT_APP_BACKEND_URL}files`, {
     method: 'POST',
     body: formData,
+    credentials: 'include',
   });
 };
